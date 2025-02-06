bc7ac84fc35b22949ae8262b534a73423b4a4d13
PrincessHornage <princesshornage@gmail.com>
2025-02-06 17:54:15 -0500

    page traversal set up and pages organized to correctly display correlating URLs

--- osaani-portfolio/app/contact/page.tsx
+++ osaani-portfolio/app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>Contact Page</div>
+    <div>This is the Contact Page</div>
   )
 }
 
--- osaani-portfolio/app/home/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectsPage = () => {
-  return (
-    <div>Projects Page</div>
-  )
-}
-
-export default ProjectsPage
