531f1814602faf444ecba5da4d7468cc9b72501f
PrincessHornage <princesshornage@gmail.com>
2025-08-13 16:14:39 -0400

    restarted project to be js native

--- job-tracker-project/job-tracker-ts/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type JobStatus = "Applied" | "Interviewing" | "Offer" | "Rejected";
-
-export interface Job {
-  id: number;
-  company: string;
-  position: string;
-  date?: string;
-  status: JobStatus;
-}
